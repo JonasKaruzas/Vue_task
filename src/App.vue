a5b71cae39b159776035464d2e616a3728f93fa1
JonasKaruzas <jonas.karuzas@gmail.com>
2023-09-15 19:46:08 +0300

    feat: :tada: Initial

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <GasPriceCard />
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import GasPriceCard from './components/GasPriceCard.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    GasPriceCard,
+  },
+  setup() {
+  
+  },
+})
+</script>
---
+++ src/components/GasPriceCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Gas Price</div>
+  <div>{{hello}}</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+  
+    const hello = 'hi'
+
+    return {
+      hello,
+    }
+  },
+})
+</script>
